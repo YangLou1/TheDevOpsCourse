9c5f0957e600cf4938dfa0d26eec30d4415b9b2d
Lili <lilixu93@icloud.com>
2018-02-20 15:43:21 -0500

    Quiz basic views and templates

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^quiz/', include('quiz.urls')),
     url(r'^devops/', include('devops.urls')),
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
+from .models import Question
 
-# Create your views here.
+
+def index(request):
+    question_list = Question.objects.all()
+    context = {'question_list': question_list}
+    return render(request, 'quiz/index.html', context)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'quiz/detail.html', {'question': question})
