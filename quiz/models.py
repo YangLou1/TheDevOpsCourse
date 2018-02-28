7e2ceb23c3d8576f31c1e965f1637b10e4599b1d
YangLou1 <bloglog@hotmail.com>
2018-02-28 12:33:51 -0500

    Return a human-readable representation of Question Answer models

--- quiz/models.py
+++ quiz/models.py
@@ -7,8 +7,14 @@ class Question(models.Model):
     topic = models.CharField(max_length=50)
     points = models.IntegerField(default=10)
 
+    def __str__(self):
+        return self.question_text
+
 
 class Answer(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     answer_text = models.CharField(max_length=200)
     correct = models.BooleanField()
+
+    def __str__(self):
+        return self.answer_text
