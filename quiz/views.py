382b0f20383b014d387ecc1e19bc873269c0bad6
YangLou1 <bloglog@hotmail.com>
2018-03-19 01:34:04 -0400

    Display all questions/answers and add result page

--- quiz/views.py
+++ quiz/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import get_object_or_404, render, HttpResponse
+from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
 from .models import Question, Answer
+
+
+score = 0
+total_points = 0
 
 
 def index(request):
@@ -8,23 +13,35 @@ def index(request):
     return render(request, 'quiz/index.html', context)
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'quiz/detail.html', {'question': question})
+def submit(request):
+    global score, total_points 
+    score = 0
+    total_points = 0
+
+    question_list = Question.objects.all()
+
+    for question in question_list:
+        try:
+            selected_answer_id = request.POST[str(question.id)]
+            selected_answer = question.answer_set.get(pk=selected_answer_id)
+        except (KeyError, Answer.DoesNotExist):
+            return render(request, 'quiz/index.html', {
+                'question_list': question_list,
+                'error_message': "Please answer all the questions before submitting.",
+            })
+        else:             
+            total_points += question.points
+            if selected_answer.correct:
+                score += question.points
+
+    return HttpResponseRedirect('result')
 
 
-def submit(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    try:
-        selected_answer = question.answer_set.get(pk=request.POST['answer'])
-    except (KeyError, Answer.DoesNotExist):
-        # Redisplay the question voting form.
-        return render(request, 'quiz/detail.html', {
-            'question': question,
-            'error_message': "You didn't select an answer.",
-        })
-    else:
-        if selected_answer.correct:
-            return HttpResponse('YES')
-        else:
-            return HttpResponse('NO')
+def result(request):   
+    global score, total_points      
+    result = {'score': score, 'total_points': total_points}   
+    score = 0
+    total_points = 0
+    return render(request, 'quiz/result.html', result)
+
+
