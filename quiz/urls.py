2537222ffb0b58e267ecc121e63000dc37a610dd
Lili <lilixu93@icloud.com>
2018-03-28 15:59:25 -0400

    Quiz page grouping questions set up

--- quiz/urls.py
+++ quiz/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),    
-    url(r'^submit$', views.submit, name='submit'),
-    url(r'^result$', views.result, name='result')
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<quiz>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<quiz>\d+)/submit$', views.submit, name='submit'),
+    url(r'^(?P<quiz>\d+)/result$', views.result, name='result')
 ]
--- quiz/views.py
+++ quiz/views.py
@@ -1,24 +1,30 @@
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .models import Question, Answer
+from .models import Question, Answer, Quiz
 
 
 def index(request):
-    question_list = Question.objects.all()
-    context = {'question_list': question_list}
+    quiz_list = Quiz.objects.all()
+    context = {'quiz_list': quiz_list}
     return render(request, 'quiz/index.html', context)
 
 
-def submit(request):
+def detail(request, quiz):
+    question_list = Question.objects.filter(quiz=quiz)
+    context = {'question_list': question_list}
+    return render(request, 'quiz/question.html', context)
+
+
+def submit(request, quiz):
     request.session['score'] = 0
     request.session['total_points'] = 0
-    question_list = Question.objects.all()
+    question_list = Question.objects.filter(quiz=quiz)
     for question in question_list:
         try:
             selected_answer_id = request.POST[str(question.id)]
             selected_answer = question.answer_set.get(pk=selected_answer_id)
         except (KeyError, Answer.DoesNotExist):
-            return render(request, 'quiz/index.html', {
+            return render(request, 'quiz/question.html', {
                 'question_list': question_list,
                 'error_message': "Please answer all the questions before submitting.",
             })
@@ -29,8 +35,11 @@ def submit(request):
     return HttpResponseRedirect('result')
 
 
-def result(request):   
-    record = {'score': request.session['score'], 'total_points': request.session['total_points']}
+def result(request, quiz):
+    record = {'quiz_id': quiz,
+              'score': request.session['score'],
+              'total_points': request.session['total_points'],
+              'pass': True if request.session['score'] >= Quiz.objects.get(id=quiz).min_pass else False}
     return render(request, 'quiz/result.html', record)
 
 
