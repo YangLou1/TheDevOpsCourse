d55bc4e5e374be7cf6deb4d95bf881751ec01604
Lili <lilixu93@icloud.com>
2018-04-23 10:42:22 -0400

    Add unit test case for Quiz model db operation

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,3 +1,12 @@
 from django.test import TestCase
+from quiz.models import Quiz
 
-# Create your tests here.
+
+class QuizTestCase(TestCase):
+    def setUp(self):
+        Quiz.objects.create(title="test")
+
+    def test_default_setting(self):
+        q = Quiz.objects.get(title="test")
+        self.assertEqual(q.num_questions, 10)
+        self.assertEqual(q.min_pass, 10)
