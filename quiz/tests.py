11ce533ec214aea6d2958fdc5fe0408987c74bde
Lili <lilixu93@icloud.com>
2018-04-24 15:31:00 -0400

    Add unit test case for Question object

--- quiz/tests.py
+++ quiz/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from quiz.models import Quiz
+from quiz.models import Quiz, Question
 
 
 class QuizTestCase(TestCase):
@@ -10,3 +10,14 @@ class QuizTestCase(TestCase):
         q = Quiz.objects.get(title="test")
         self.assertEqual(q.num_questions, 10)
         self.assertEqual(q.min_pass, 10)
+
+
+class QuestionTestCase(TestCase):
+    def setUp(self):
+        mocked_quiz = Quiz.objects.create(title="test")
+        Question.objects.create(quiz=mocked_quiz, question_text="test")
+
+    def test_default_setting(self):
+        mocked_quiz = Quiz.objects.get(title="test")
+        question = Question.objects.get(quiz=mocked_quiz)
+        self.assertEqual(question.question_text, "test")
